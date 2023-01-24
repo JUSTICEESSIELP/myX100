efff430f82e3c2c372e58f0b90c36c4411255f06
JUSTICEESIELP <justicessiel@gmail.com>
2023-01-24 11:39:36 +0000

    first commit

---
+++ src/components/form fields/textarea/ant/tf-basic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TFBasic = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TFShadow;
---
+++ src/components/form fields/textarea/ant/tf.shadow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TFShadow = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TFShadow;
